21092863799df5b92e93eb6fdf23671f7d3c235c
svetaosmonova <svetaosmonova.83@gmail.com>
2025-03-16 22:23:49 +0300

    lesson4

--- 05_lesson/Add.py
+++ 05_lesson/Add.py
@@ -1,24 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
+# Шаг 1: Открываем страницу
+driver = webdriver.Chrome()
+driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 
-
-url = "http://the-internet.herokuapp.com/add_remove_elements/"
-browser = webdriver.Chrome()
-browser.get(url)
-
-
-add_button = browser.find_element(By.XPATH, "//button[text()='Add Element']")
+# Шаг 2: Пять раз кликаем на кнопку Add Element
+add_button = driver.find_element(By.CSS_SELECTOR, "#content > div > button")
 for _ in range(5):
     add_button.click()
-    time.sleep(1)
 
+# Шаг 3: Собираем список кнопок Delete
+delete_buttons = driver.find_elements(By.XPATH, "//button[text()='Delete']")
 
-delete_buttons = browser.find_elements(By.XPATH, "//button[text()='Delete']")
+# Шаг 4: Выводим на экран размер списка
+print(f"Количество кнопок 'Delete': {len(delete_buttons)}")
 
-
-print(f"Количество кнопок Delete: {len(delete_buttons)}")
-
-
-browser.quit()
+# Закрытие браузера (необязательно)
+driver.quit()
