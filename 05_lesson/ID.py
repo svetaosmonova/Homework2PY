ba8fbcf3c942a3effb287655d23b82ea24423fc9
svetaosmonova <svetaosmonova.83@gmail.com>
2025-03-17 19:43:02 +0300

    Клик по кнопке без ID

--- 05_lesson/ID.py
+++ 05_lesson/ID.py
@@ -1,32 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/dynamicid")
 
-def click_blue_button():
+# Используем класс для поиска кнопки
+blue_button = driver.find_element(By.CLASS_NAME, "btn-primary")
+blue_button.click()
 
-    driver = webdriver.Chrome()
-    driver.get("http://uitestingplayground.com/dynamicid")
-
-
-    try:
-
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "button0"))
-        )
-
-
-        blue_button = driver.find_element(By.ID, "button0")
-        blue_button.click()
-
-        print("Синяя кнопка найдена и нажата!")
-    except Exception as e:
-        print(f"Произошла ошибка: {str(e)}")
-    finally:
-
-        driver.quit()
-
-
-if __name__ == "__main__":
-    click_blue_button()
+print("Синяя кнопка найдена и нажата!")
+driver.quit()
