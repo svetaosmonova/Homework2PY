d4aec8cbc00d62b06e7f8513d75468dd79c06e78
svetaosmonova <svetaosmonova.83@gmail.com>
2025-03-18 06:32:53 +0300

    Клик по кнопке с CSS-классом

--- 05_lesson/CSS.py
+++ 05_lesson/CSS.py
@@ -1,32 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 def click_blue_button():
-
     driver = webdriver.Chrome()
     driver.get("http://uitestingplayground.com/classattr")
 
 
-    try:
+    blue_button = driver.find_element(By.CLASS_NAME, "btn-primary")
+    blue_button.click()
 
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "btn-primary"))
-        )
-
-
-        blue_button = driver.find_element(By.CLASS_NAME, "btn-primary")
-        blue_button.click()
-
-        print("Синяя кнопка найдена и нажата!")
-    except Exception as e:
-        print(f"Произошла ошибка: {str(e)}")
-    finally:
-
-        driver.quit()
+    print("Синяя кнопка найдена и нажата!")
+    driver.quit()
 
 
-if __name__ == "__main__":
-    click_blue_button()
+click_blue_button()
