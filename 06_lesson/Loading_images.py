2e28f7e2219e13bb53c2177223a7f5017b746087
svetaosmonova <svetaosmonova.83@gmail.com>
2025-03-25 20:12:48 +0300

    Дождаться картинки

--- 06_lesson/Loading_images.py
+++ 06_lesson/Loading_images.py
@@ -7,22 +7,19 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+wait = WebDriverWait(driver, 10)
+images = wait.until(
+    EC.visibility_of_all_elements_located((By.TAG_NAME, 'img'))
+)
 
-wait = WebDriverWait(driver, 10)  # Ожидание в течение 10 секунд
-order = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#award")))
+wait = WebDriverWait(driver, 30)
+order = wait.until(EC.presence_of_element_located((By.ID, "award")))
 
 
-
-order = driver.find_element(By.ID, "award")
-
-# Получение атрибута "src" у найденного элемента
 src_value = order.get_attribute("src")
 
-# Вывод значения атрибута "src" в консоль
+
 print(src_value)
 
-# Закрытие браузера
+р
 driver.quit()
-
