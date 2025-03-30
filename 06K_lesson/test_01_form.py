32f03d4123cc0b06eaf5320fcae9167397c6ee59
svetaosmonova <svetaosmonova.83@gmail.com>
2025-03-30 11:02:31 +0300

    Контрольная работа

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -1,10 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
 
 
 @pytest.fixture(scope="module")
@@ -15,25 +11,32 @@ def driver():
     driver.quit()
 
 
-
 def test_shopping_cart(driver):
 
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+    driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);")
 
-
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-
-    first_name_field = driver.find_element(By.NAME, 'first-name')
-    last_name_field = driver.find_element(By.NAME, 'last-name')
-    address_field = driver.find_element(By.NAME, 'address')
-    email_field = driver.find_element(By.NAME, 'e-mail')
-    phone_number_field = driver.find_element(By.NAME, 'phone')
-    city_field = driver.find_element(By.NAME, 'city')
-    country_field = driver.find_element(By.NAME, 'country')
-    job_position_field = driver.find_element(By.NAME, 'job-position')
-    company_field = driver.find_element(By.NAME, 'company')
+    first_name_field = driver.find_element(
+        By.NAME, 'first-name')
+    last_name_field = driver.find_element(
+        By.NAME, 'last-name')
+    address_field = driver.find_element(
+        By.NAME, 'address')
+    email_field = driver.find_element(
+        By.NAME, 'e-mail')
+    phone_number_field = driver.find_element(
+        By.NAME, 'phone')
+    city_field = driver.find_element(
+        By.NAME, 'city')
+    country_field = driver.find_element(
+        By.NAME, 'country')
+    job_position_field = driver.find_element(
+        By.NAME, 'job-position')
+    company_field = driver.find_element(
+        By.NAME, 'company')
 
     first_name_field.send_keys('Иван')
     last_name_field.send_keys('Петров')
@@ -45,17 +48,28 @@ def test_shopping_cart(driver):
     job_position_field.send_keys('QA')
     company_field.send_keys('SkyPro')
 
-
-    submit_button = driver.find_element(By.XPATH, '//button[contains(text(), "Submit")]')
+    submit_button = driver.find_element(
+        By.XPATH, '//button[contains(text(), "Submit")]'
+    )
     submit_button.click()
 
-    assert "danger" in driver.find_element(By.ID, "zip-code").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "first-name").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "last-name").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "address").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "e-mail").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "phone").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "city").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "country").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "job-position").get_attribute("class")
-    assert "success" in driver.find_element(By.ID, "company").get_attribute("class")
+    assert "danger" in driver.find_element(
+        By.ID, "zip-code").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "first-name").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "last-name").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "address").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "e-mail").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "phone").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "city").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "country").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "job-position").get_attribute("class")
+    assert "success" in driver.find_element(
+        By.ID, "company").get_attribute("class")
--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 
 @pytest.fixture(scope="module")
@@ -18,47 +17,59 @@ def test_shopping_cart(driver):
     # Открытие сайта магазина
     driver.get("https://www.saucedemo.com/")
 
-
-
     # Авторизация
-    username_input = driver.find_element(By.ID, "user-name")
-    password_input = driver.find_element(By.ID, "password")
-    login_button = driver.find_element(By.CLASS_NAME, "btn_action")
+    username_input = driver.find_element(
+        By.ID, "user-name")
+    password_input = driver.find_element(
+        By.ID, "password")
+    login_button = driver.find_element(
+        By.CLASS_NAME, "btn_action")
 
     username_input.send_keys("standard_user")
     password_input.send_keys("secret_sauce")
     login_button.click()
 
-
     # Добавление товаров в корзину
-    backpack_add_button = driver.find_element(By.XPATH,
-                                              "//div[contains(text(), 'Sauce Labs Backpack')]/ancestor::div[@class='inventory_item']/descendant::button")
-    tshirt_add_button = driver.find_element(By.XPATH,
-                                            "//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]/ancestor::div[@class='inventory_item']/descendant::button")
-    onesie_add_button = driver.find_element(By.XPATH,
-                                            "//div[contains(text(), 'Sauce Labs Onesie')]/ancestor::div[@class='inventory_item']/descendant::button")
+    backpack_add_button = driver.find_element(
+        By.XPATH,
+        "//div[contains(text(), 'Sauce Labs Backpack')]/"
+        "ancestor::div[@class='inventory_item']/"
+        "descendant::button"
+    )
+    tshirt_add_button = driver.find_element(
+        By.XPATH,
+        "//div[contains(text(), 'Sauce Labs Bolt T-Shirt')]/"
+        "ancestor::div[@class='inventory_item']/"
+        "descendant::button"
+    )
+    onesie_add_button = driver.find_element(
+        By.XPATH,
+        "//div[contains(text(), 'Sauce Labs Onesie')]/"
+        "ancestor::div[@class='inventory_item']/"
+        "descendant::button"
+    )
 
     backpack_add_button.click()
     tshirt_add_button.click()
     onesie_add_button.click()
 
-    wait = WebDriverWait(driver, 20)
-
-
     # Переход в корзину
-    cart_icon = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+    cart_icon = driver.find_element(
+        By.CLASS_NAME, "shopping_cart_link")
     cart_icon.click()
 
     # Нажатие кнопки Checkout
-    checkout_button = driver.find_element(By.CLASS_NAME, "checkout_button")
+    checkout_button = driver.find_element(
+        By.CLASS_NAME, "checkout_button")
     checkout_button.click()
 
-
-
     # Заполнение формы данными
-    first_name_input = driver.find_element(By.ID, "first-name")
-    last_name_input = driver.find_element(By.ID, "last-name")
-    postal_code_input = driver.find_element(By.ID, "postal-code")
+    first_name_input = driver.find_element(
+        By.ID, "first-name")
+    last_name_input = driver.find_element(
+        By.ID, "last-name")
+    postal_code_input = driver.find_element(
+        By.ID, "postal-code")
 
     first_name_input.send_keys("Имя")
     last_name_input.send_keys("Фамилия")
@@ -69,15 +80,14 @@ def test_shopping_cart(driver):
             By.CSS_SELECTOR, "#continue"))
     ).click()
 
-
-
-
     # Чтение итоговой стоимости
-    total_cost = driver.find_element(By.CLASS_NAME, "summary_total_label").text
+    total_cost = driver.find_element(
+        By.CLASS_NAME, "summary_total_label").text
 
-
-
-    assert total_cost == "Total: $58.29", f"Итоговая сумма не совпадает с ожидаемой. Ожидается: $58.29, Получено: ${total_cost}"
+    assert (
+            total_cost == "Total: $58.29"
+    ), (f"Итоговая сумма не совпадает с ожидаемой. "
+        f"Ожидается: $58.29, Получено: ${total_cost}")
 
     # Закрытие браузера
     driver.quit()
