15e279d21fe1ef7e3bf823182a7a69e46ed5d3ec
svetaosmonova <svetaosmonova.83@gmail.com>
2025-04-08 08:35:41 +0300

    калькулятор

---
+++ 7_less/Calculator/test_calculator.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from Calc_class_test import Calculator
+
+
+@pytest.fixture(scope="module")
+def driver():
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(3)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def test_shopping_cart(driver):
+    calculator = Calculator(driver)
+    calculator.set_delay(1)
+    calculator.click_button('7')
+    calculator.click_button('+')
+    calculator.click_button('8')
+    calculator.click_button('=')
+
+    calculator.wait_until_result_is("15")
+    assert calculator.get_result_text() == "15"
